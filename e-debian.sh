a0153f06f8da08041012283169ba9c06e96fba7c
FabTheGithubUser <76869047+FabTheGithubUser@users.noreply.github.com>
2021-12-21 15:33:08 +0100

    Update e-debian.sh

--- e-debian.sh
+++ e-debian.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo apt update
 sudo apt dist-upgrade
-sudo apt install desktop-base tasksel xorg xserver-xorg-input-all xserver-xorg-video-all alsa-utils anacron avahi-daemon eject firefox-esr fonts-symbola iw sudo lightdm lightdm-gtk-greeter enlightenment terminology pcmanfm vim emacs mousepad pulseaudio connman connman-ui connman-gtk synaptic xdg-user-dirs
+sudo apt install desktop-base tasksel xorg xserver-xorg-input-all xserver-xorg-video-all alsa-utils anacron avahi-daemon eject firefox-esr fonts-symbola iw sudo lightdm lightdm-gtk-greeter enlightenment terminology pcmanfm vim mousepad pulseaudio connman connman-ui connman-gtk synaptic xdg-user-dirs
 # Recommended: reboot!
 xdg-user-dirs-update
+sudo apt install abiword gnumeric vlc galculator gdebi gdebi-core deluge
 echo "Have fun."
